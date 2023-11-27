f6b7e674b191be2670795e5c21d103aa3c98f729
Thomas Ebner <96168670+samohte@users.noreply.github.com>
2023-11-27 10:51:46 +0100

    Feat/dynatrace sampler internal (#11)
    Signed-off-by: thomas.ebner <thomas.ebner@dynatrace.com>

---
+++ test/extensions/tracers/opentelemetry/samplers/dynatrace/dynatrace_tracestate_test.cc
@@ -0,0 +1,21 @@
+#include <string>
+
+#include "source/extensions/tracers/opentelemetry/samplers/dynatrace/dynatrace_tracestate.h"
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+namespace Envoy {
+namespace Extensions {
+namespace Tracers {
+namespace OpenTelemetry {
+
+TEST(DynatraceTracestateTest, TestKey) {
+  DtTracestateEntry tracestate("98812ad49", "980df25c");
+  EXPECT_STREQ(tracestate.getKey().c_str(), "98812ad49-980df25c@dt");
+}
+
+} // namespace OpenTelemetry
+} // namespace Tracers
+} // namespace Extensions
+} // namespace Envoy
