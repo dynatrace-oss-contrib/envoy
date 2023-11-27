3400b8e66001d312338d6285d73ca28b21386bdc
Thomas Ebner <96168670+samohte@users.noreply.github.com>
2023-11-27 10:51:46 +0100

    Feat/dynatrace sampler internal (#11)
    Signed-off-by: thomas.ebner <thomas.ebner@dynatrace.com>

---
+++ source/extensions/tracers/opentelemetry/samplers/dynatrace/config.cc
@@ -0,0 +1,34 @@
+#include "config.h"
+
+#include "envoy/extensions/tracers/opentelemetry/samplers/v3/dynatrace_sampler.pb.validate.h"
+
+#include "source/common/config/utility.h"
+#include "source/common/protobuf/utility.h"
+#include "source/extensions/tracers/opentelemetry/samplers/dynatrace/dynatrace_sampler.h"
+
+namespace Envoy {
+namespace Extensions {
+namespace Tracers {
+namespace OpenTelemetry {
+
+SamplerSharedPtr
+DynatraceSamplerFactory::createSampler(const Protobuf::Message& config,
+                                       Server::Configuration::TracerFactoryContext& context) {
+  auto mptr = Envoy::Config::Utility::translateAnyToFactoryConfig(
+      dynamic_cast<const ProtobufWkt::Any&>(config), context.messageValidationVisitor(), *this);
+  return std::make_shared<DynatraceSampler>(
+      MessageUtil::downcastAndValidate<
+          const envoy::extensions::tracers::opentelemetry::samplers::v3::DynatraceSamplerConfig&>(
+          *mptr, context.messageValidationVisitor()),
+      context);
+}
+
+/**
+ * Static registration for the Env sampler factory. @see RegisterFactory.
+ */
+REGISTER_FACTORY(DynatraceSamplerFactory, SamplerFactory);
+
+} // namespace OpenTelemetry
+} // namespace Tracers
+} // namespace Extensions
+} // namespace Envoy
---
+++ source/extensions/tracers/opentelemetry/samplers/dynatrace/dynatrace_sampler.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "envoy/extensions/tracers/opentelemetry/samplers/v3/dynatrace_sampler.pb.h"
+#include "envoy/server/factory_context.h"
+
+#include "source/common/common/logger.h"
+#include "source/common/config/datasource.h"
+#include "source/extensions/tracers/opentelemetry/samplers/dynatrace/dynatrace_tracestate.h"
+#include "source/extensions/tracers/opentelemetry/samplers/dynatrace/tracestate.h"
+#include "source/extensions/tracers/opentelemetry/samplers/sampler.h"
+
+namespace Envoy {
+namespace Extensions {
+namespace Tracers {
+namespace OpenTelemetry {
+
+/**
+ * @brief A Dynatrace specific sampler *
+ */
+class DynatraceSampler : public Sampler, Logger::Loggable<Logger::Id::tracing> {
+public:
+  DynatraceSampler(
+      const envoy::extensions::tracers::opentelemetry::samplers::v3::DynatraceSamplerConfig& config,
+      Server::Configuration::TracerFactoryContext& context);
+
+  SamplingResult shouldSample(const absl::optional<SpanContext> parent_context,
+                              const std::string& trace_id, const std::string& name,
+                              OTelSpanKind spankind,
+                              OptRef<const Tracing::TraceContext> trace_context,
+                              const std::vector<SpanContext>& links) override;
+
+  std::string getDescription() const override;
+
+private:
+  std::string tenant_id_;
+  std::string cluster_id_;
+  DtTracestateEntry dt_tracestate_entry_;
+  std::atomic<uint32_t> counter_; // request counter for dummy sampling
+  FW4Tag getFW4Tag(const Tracestate& tracestate);
+};
+
+} // namespace OpenTelemetry
+} // namespace Tracers
+} // namespace Extensions
+} // namespace Envoy
