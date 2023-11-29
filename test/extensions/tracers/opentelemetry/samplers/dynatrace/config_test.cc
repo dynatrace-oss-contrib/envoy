f3477d754b2369d676ef49e45a7752eb26b34377
thomas.ebner <thomas.ebner@dynatrace.com>
2023-11-29 06:34:14 +0100

    fix dynatrace sampler unit test
    Signed-off-by: thomas.ebner <thomas.ebner@dynatrace.com>

--- test/extensions/tracers/opentelemetry/samplers/dynatrace/config_test.cc
+++ test/extensions/tracers/opentelemetry/samplers/dynatrace/config_test.cc
@@ -20,14 +20,30 @@ TEST(DynatraceSamplerFactoryTest, Test) {
   EXPECT_NE(factory->createEmptyConfigProto(), nullptr);
 
   envoy::config::core::v3::TypedExtensionConfig typed_config;
-  const std::string yaml = R"EOF(
+  const std::string sampler_yaml = R"EOF(
     name: envoy.tracers.opentelemetry.samplers.dynatrace
     typed_config:
         "@type": type.googleapis.com/envoy.extensions.tracers.opentelemetry.samplers.v3.DynatraceSamplerConfig
   )EOF";
-  TestUtility::loadFromYaml(yaml, typed_config);
+  TestUtility::loadFromYaml(sampler_yaml, typed_config);
+
+  const std::string otel_yaml = R"EOF(
+    http_service:
+      http_uri:
+        cluster: "my_o11y_backend"
+        uri: "https://some-o11y.com/otlp/v1/traces"
+        timeout: 0.250s
+      request_headers_to_add:
+      - header:
+          key: "Authorization"
+          value: "auth-token"
+    )EOF";
+  envoy::config::trace::v3::OpenTelemetryConfig opentelemetry_config;
+  TestUtility::loadFromYaml(otel_yaml, opentelemetry_config);
+
   NiceMock<Server::Configuration::MockTracerFactoryContext> context;
-  EXPECT_NE(factory->createSampler(typed_config.typed_config(), context), nullptr);
+  EXPECT_NE(factory->createSampler(typed_config.typed_config(), context, opentelemetry_config),
+            nullptr);
   EXPECT_STREQ(factory->name().c_str(), "envoy.tracers.opentelemetry.samplers.dynatrace");
 }
 
