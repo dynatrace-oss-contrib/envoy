bc072af7fc5b09864285fee19042aa0a836e5dc6
thomas.ebner <thomas.ebner@dynatrace.com>
2024-02-12 12:51:56 +0100

    review feedback
    Signed-off-by: thomas.ebner <thomas.ebner@dynatrace.com>

--- test/extensions/tracers/opentelemetry/samplers/dynatrace/sampler_config_fetcher_test.cc
+++ test/extensions/tracers/opentelemetry/samplers/dynatrace/sampler_config_fetcher_test.cc
@@ -67,7 +67,7 @@ MATCHER_P(MessageMatcher, unusedArg, "") {
 TEST_F(SamplerConfigFetcherTest, TestRequestIsSent) {
   EXPECT_CALL(tracer_factory_context_.server_factory_context_.cluster_manager_.thread_local_cluster_
                   .async_client_,
-              send_(MessageMatcher("unused"), _, _));
+              send_(MessageMatcher("unused-arg"), _, _));
   SamplerConfigFetcherImpl config_fetcher(tracer_factory_context_, http_uri_, "tokenval");
   timer_->invokeCallback();
 }
